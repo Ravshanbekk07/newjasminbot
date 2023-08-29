c0719f1610185c2e69ae623208b47acbe35e8739
Ravshanbekk07 <ramazonof07@gmail.com>
2023-08-29 10:54:40 +0500

    token

--- bot.py
+++ bot.py
@@ -5,3 +5,18 @@ import os
 from handlers import (start)
 
 from flask import Flask,request
+TOKEN=os.environ.get('TOKEN')
+bot =Bot(TOKEN)
+
+app=Flask(__name__)
+
+@app.route('/webhook/',methods=['POST'])
+def main():
+    dis=Dispatcher(bot,None,workers=0)
+    data=request.get_json(force=True)
+    update=Update.de_json(data=data,bot=bot)
+    dis.add_handler(handler=Commandhandler('start',start))
+
+
+    dis.process_update(update)
+    return 'cool'
